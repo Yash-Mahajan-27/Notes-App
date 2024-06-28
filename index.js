c824e242e9abf9c71723747e0b73b5a64a03c7dd
Yash-Mahajan-27 <mahajanyash15@gmail.com>
2024-06-28 16:11:31 +0530

    first commit

--- index.js
+++ index.js
@@ -22,10 +22,39 @@ app.get('/file/:filename',(req,res)=>{
     
 })
 
+app.get('/edit/:filename',(req,res)=>{
+    res.render('edit',{filename: req.params.filename})
+})
+
+app.post('/edit',(req,res)=>{
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new.split(' ').map(word => word.charAt(0).toUpperCase()+ word.slice(1)).join('')}.txt`,(err)=>{
+        res.redirect("/");
+    })
+})
+
 app.post('/create',(req,res)=>{
     fs.writeFile(`./files/${req.body.title.split(' ').map(word => word.charAt(0).toUpperCase()+ word.slice(1)).join('')}.txt`,req.body.details,(err)=>{
         res.redirect("/");
     })
 })
 
+
+app.get('/delete/:filename',(req,res)=>{
+    res.render('delete',{filename: req.params.filename})
+})
+
+
+
+app.post('/delete/:filename', (req, res) => {
+    const filePath = path.join(__dirname, 'files', req.params.filename); // Ensure the correct file path
+
+    fs.unlink(filePath, (err) => {
+        res.redirect('/');
+    });
+}); 
+
+
+
 app.listen(3000); 
+
+
